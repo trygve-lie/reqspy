aae1eda39bdf61e1e1d21b8a1d9c1ca6bfa2fb02
Trygve Lie <post@trygve-lie.com>
2020-01-17 21:56:53 +0100

    Rewritten to emit a metric stream

--- example/example.js
+++ example/example.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { Writable } = require('stream');
 const https = require('https');
 const Spy = require('../');
 
@@ -9,6 +10,16 @@ spy.enable();
 spy.on('host', (info) => {
     console.log('new host requested', info);
 })
+
+const to = new Writable({
+    objectMode: true,
+    write(chunk, encoding, callback) {
+        console.log(chunk);
+        callback();
+    }
+})
+
+spy.metrics.pipe(to);
 
 setTimeout(() => {
     https.get('https://www.finn.no', () => {
--- test/spy.js
+++ test/spy.js
@@ -1,182 +1,217 @@
 'use strict';
 
+const { Writable } = require('stream');
 const { test } = require('tap');
 const https = require('https');
 const Spy = require('../');
 
 const get = (url) => {
     return new Promise((resolve) => {
-        https.get(url, resolve).on('error', resolve);
+        https.get(url, () => {
+            process.nextTick(resolve);
+        }).on('error', () => {
+            process.nextTick(resolve);
+        });
     });
 };
 
+const to = (done) => {
+    const items = [];
+    const writer = new Writable({
+        objectMode: true,
+        write(chunk, encoding, callback) {
+            items.push(chunk);
+            callback();
+        }
+    });
+    writer.on('finish', () => {
+        done(items);
+    });
+    return writer;
+};
+
 test('Object type', (t) => {
-    const spy = new Spy();
+    const spy = new Spy({ hostname: 'turing' });
     t.equal(Object.prototype.toString.call(spy), '[object ReqSpy]', 'should be ReqSpy');
     t.end();
 });
 
-test('The spy is not enabled', async (t) => {
-    const spy = new Spy();
-
-    await Promise.all([
-        get('https://github.com'),
-        get('https://github.com'),
-        get('https://google.com'),
-    ]);
-
-    t.equal(spy.values().length, 0, 'should not log any hosts');
+test('No arguments', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const spy = new Spy(); // eslint-disable-line no-unused-vars
+    }, /The argument "hostname" must be provided/, 'should throw');
     t.end();
 });
 
-test('The spy is enabled', async (t) => {
-    const spy = new Spy();
-    spy.enable();
-
-    await Promise.all([
-        get('https://github.com'),
-        get('https://github.com'),
-        get('https://google.com'),
-    ]);
-
-    t.equal(spy.values().length, 2, 'should log two hosts');
-    t.end();
-});
-
-test('The spy is enabled, then disabled', async (t) => {
-    const spy = new Spy();
-    spy.enable();
-
-    await Promise.all([
-        get('https://github.com'),
-        get('https://github.com'),
-    ]);
-
-    t.equal(spy.values().length, 1, 'should have logged one host');
-
-    spy.disable();
-    await get('https://google.com');
-
-    t.equal(spy.values().length, 1, 'should have logged one host');
-    t.end();
-});
-
-test('The spy is cleared', async (t) => {
-    const spy = new Spy();
-    spy.enable();
-
-    await Promise.all([
-        get('https://github.com'),
-        get('https://github.com'),
-        get('https://google.com'),
-    ]);
-
-    t.equal(spy.values().length, 2, 'should log two hosts');
-
-    spy.clear();
-
-    t.equal(spy.values().length, 0, 'should not have any hosts logged');
-    t.end();
-});
-
-test('Recorded values on resolved host', async (t) => {
-    const spy = new Spy();
-    spy.enable();
-
-    await get('https://github.com');
-
-    const hosts = spy.values();
-
-    t.type(hosts[0].hostname, 'string', '".hostname" should be a Sting');
-    t.type(hosts[0].address, 'string', '".address" should be a Sting');
-    t.type(hosts[0].family, 'number', '".family" should be a Number');
-    t.type(hosts[0].error, 'boolean', '".error" should be a Boolean');
-    t.false(hosts[0].error, '".error" should be "false"');
-    t.end();
-});
-
-test('Recorded values on un-resolved host', async (t) => {
-    const spy = new Spy();
-    spy.enable();
-
-    await get('https://klfsdjngkjdfljs.com');
-
-    const hosts = spy.values();
-
-    t.type(hosts[0].hostname, 'string', '".hostname" should be a Sting');
-    t.type(hosts[0].address, 'null', '".address" should be a null');
-    t.type(hosts[0].family, 'null', '".family" should be a null');
-    t.type(hosts[0].error, 'boolean', '".error" should be a Boolean');
-    t.true(hosts[0].error, '".error" should be "true"');
-    t.end();
-});
-
-test('.toJSON on resolved host', async (t) => {
-    const spy = new Spy();
-    spy.enable();
-
-    await get('https://github.com');
-
-    const str = JSON.stringify(spy);
-    const obj = JSON.parse(str);
-
-    t.type(obj.hosts[0].hostname, 'string', '".hostname" should be a Sting');
-    t.type(obj.hosts[0].address, 'string', '".address" should be a Sting');
-    t.type(obj.hosts[0].family, 'number', '".family" should be a Number');
-    t.type(obj.hosts[0].error, 'boolean', '".error" should be a Boolean');
-    t.false(obj.hosts[0].error, '".error" should be "false"');
-    t.end();
-});
-
-test('.toJSON on un-resolved host', async (t) => {
-    const spy = new Spy();
-    spy.enable();
-
-    await get('https://klfsdjngkjdfljs.com');
-
-    const str = JSON.stringify(spy);
-    const obj = JSON.parse(str);
-
-    t.type(obj.hosts[0].hostname, 'string', '".hostname" should be a Sting');
-    t.type(obj.hosts[0].address, 'null', '".address" should be a null');
-    t.type(obj.hosts[0].family, 'null', '".family" should be a null');
-    t.type(obj.hosts[0].error, 'boolean', '".error" should be a Boolean');
-    t.true(obj.hosts[0].error, '".error" should be "true"');
+test('The argument "metricName" is not legal', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const spy = new Spy({ hostname: 'turing', metricName: {} }); // eslint-disable-line no-unused-vars
+    }, /Provided value to argument "metricName" is not legal/, 'should throw');
     t.end();
 });
 
 test('"host" event on resolved host', async (t) => {
-    const spy = new Spy();
+    const spy = new Spy({ hostname: 'turing' });
     spy.enable();
 
+    let result = {};
+
     spy.once('host', (obj) => {
-        t.type(obj.hostname, 'string', '".hostname" should be a Sting');
-        t.type(obj.address, 'string', '".address" should be a Sting');
-        t.type(obj.family, 'number', '".family" should be a Number');
-        t.type(obj.error, 'boolean', '".error" should be a Boolean');
-        t.false(obj.error, '".error" should be "false"');
-        t.end();
+        result = obj;
     });
 
     await get('https://github.com');
+
+    t.type(result.hostname, 'string', '".hostname" should be a Sting');
+    t.type(result.address, 'string', '".address" should be a Sting');
+    t.type(result.family, 'number', '".family" should be a Number');
+    t.type(result.error, 'boolean', '".error" should be a Boolean');
+    t.false(result.error, '".error" should be "false"');
+    t.end();
 });
 
-test('.toJSON on un-resolved host', async (t) => {
-    const spy = new Spy();
+test('"host" event on un-resolved host', async (t) => {
+    const spy = new Spy({ hostname: 'turing' });
     spy.enable();
 
+    let result = {};
+
     spy.once('host', (obj) => {
-        t.type(obj.hostname, 'string', '".hostname" should be a Sting');
-        t.type(obj.address, 'null', '".address" should be a null');
-        t.type(obj.family, 'null', '".family" should be a null');
-        t.type(obj.error, 'boolean', '".error" should be a Boolean');
-        t.true(obj.error, '".error" should be "true"');
-        t.end();
+        result = obj;
     });
 
     await get('https://klfsdjngkjdfljs.com');
+
+    t.type(result.hostname, 'string', '".hostname" should be a Sting');
+    t.type(result.address, 'null', '".address" should be a null');
+    t.type(result.family, 'null', '".family" should be a null');
+    t.type(result.error, 'boolean', '".error" should be a Boolean');
+    t.true(result.error, '".error" should be "true"');
+    t.end();
 });
 
+test('The spy is not enabled', async (t) => {
+    const spy = new Spy({ hostname: 'turing' });
+
+    let result = [];
+
+    spy.on('host', (obj) => {
+        result.push(obj);
+    });
+
+    await Promise.all([
+        get('https://github.com'),
+        get('https://github.com'),
+        get('https://google.com'),
+    ]);
+
+    t.equal(result.length, 0, 'should not log any hosts');
+    t.end();
+});
+
+test('The spy is enabled, then disabled', async (t) => {
+    const spy = new Spy({ hostname: 'turing' });
+    spy.enable();
+
+    let result = [];
+
+    spy.on('host', (obj) => {
+        result.push(obj);
+    });
+
+    await Promise.all([
+        get('https://github.com'),
+        get('https://github.com'),
+    ]);
+
+    t.equal(result.length, 2, 'should have logged two hosts');
+
+    spy.disable();
+    await get('https://google.com');
+
+    t.equal(result.length, 2, 'should have logged two hosts');
+    t.end();
+});
+
+test('Recorded values on resolved host', async (t) => {
+    const spy = new Spy({ hostname: 'turing' });
+    spy.enable();
+
+    let result = [];
+
+    spy.on('host', (obj) => {
+        result.push(obj);
+    });
+
+    await get('https://github.com');
+
+    t.type(result[0].hostname, 'string', '".hostname" should be a Sting');
+    t.type(result[0].address, 'string', '".address" should be a Sting');
+    t.type(result[0].family, 'number', '".family" should be a Number');
+    t.type(result[0].error, 'boolean', '".error" should be a Boolean');
+    t.false(result[0].error, '".error" should be "false"');
+    t.end();
+});
+
+test('Recorded values on un-resolved host', async (t) => {
+    const spy = new Spy({ hostname: 'turing' });
+    spy.enable();
+
+    let result = [];
+
+    spy.on('host', (obj) => {
+        result.push(obj);
+    });
+
+    await get('https://klfsdjngkjdfljs.com');
+
+    t.type(result[0].hostname, 'string', '".hostname" should be a Sting');
+    t.type(result[0].address, 'null', '".address" should be a null');
+    t.type(result[0].family, 'null', '".family" should be a null');
+    t.type(result[0].error, 'boolean', '".error" should be a Boolean');
+    t.true(result[0].error, '".error" should be "true"');
+    t.end();
+});
+
+test('metric stream', async (t) => {
+    const spy = new Spy({ hostname: 'turing' });
+    spy.enable();
+
+    const dest = to((result) => {
+        t.equal(result.length, 3, 'should emit a metric for each host');
+        t.end();
+    });
+    spy.metrics.pipe(dest);
+
+    await Promise.all([
+        get('https://github.com'),
+        get('https://github.com'),
+        get('https://google.com'),
+    ]);
+
+    dest.end();
+});
+
+test('metric stream values', async (t) => {
+    const spy = new Spy({ metricName: 'alan', hostname: 'turing' });
+    spy.enable();
+
+    const dest = to((result) => {
+        t.equal(result[0].name, 'alan', 'should have same value as "metricName" on constructor');
+        t.equal(result[0].labels.length, 5, 'should have labels');
+        t.end();
+    });
+    spy.metrics.pipe(dest);
+
+    await get('https://github.com');
+
+    dest.end();
+});
+
+
+/*
 test('Argument "hostname" is not set on constructor', (t) => {
     const spy = new Spy();
 
@@ -196,3 +231,4 @@ test('Argument "hostname" is set on constructor', (t) => {
     t.equal(obj.hostname, 'foobar', 'should be the same as set on the constructor');
     t.end();
 });
+*/
